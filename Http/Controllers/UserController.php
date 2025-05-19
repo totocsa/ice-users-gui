5edb6a757c3d2889619d9eab4ee53b3b50d56e60
Csaba Tóth <totocsa@gmail.com>
2025-05-19 18:39:03 +0200

    Do not publish ice-icsesd-resources

--- Http/Controllers/UserController.php
+++ Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace Totocsa\UsersGUI\Http\Controllers;
 
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Str;
 use Totocsa\Icseusd\Http\Controllers\IcseusdController;
 use App\Models\User;
 
@@ -177,6 +178,20 @@ class UserController extends IcseusdController
         return [
             'current_team_idValueTexts' => $current_team_idValueTexts,
         ];
+    }
+
+    public function setVueComponents($prefix = '')
+    {
+        $name = last(explode('\\', $this::class));
+        $name = substr($name, 0, strlen($name) - strlen('Controller'));
+        $prefix .= Str::plural($name);
+
+        $routes = $this->getRoutes();
+
+        $this->vueComponents = [];
+        foreach ($routes as $k => $v) {
+            $this->vueComponents[$k] = "../../../vendor/totocsa/ice-users-gui/resources/js/Pages/$prefix/" . ucfirst(strtolower($k));
+        }
     }
 
     public function getCurrent_team_idValueTexts()
