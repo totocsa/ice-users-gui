6e7abd4e3a136cbfc20dba7d7f7da50a4c669ef3
Csaba Tóth <totocsa@gmail.com>
2025-05-22 03:54:40 +0200

    Changing if ($this->filters[$k] > 0) to if ($this->filters[$k] > '')

--- Http/Controllers/UserController.php
+++ Http/Controllers/UserController.php
@@ -137,7 +137,7 @@ class UserController extends IcseusdController
             ]);
 
         foreach ($this->conditions() as $k => $v) {
-            if ($this->filters[$k] > 0) {
+            if ($this->filters[$k] > '') {
                 $cond = $this->conditions()[$k];
                 $value = strtr($cond['value'], $this->replaceFieldToValue());
                 $query->where(str_replace('-', '.', $k), $cond['operator'], $value, $cond['boolean']);
