2ac9c10f854eae1445a80a48009c61be34a4741d
Csaba Tóth <totocsa@gmail.com>
2025-05-24 13:24:59 +0200

    = MigrationHelper::stubsToMigrations(, __DIR__ . '/database/migrations/');

--- UsersGUIServiceProvider.php
+++ UsersGUIServiceProvider.php
@@ -18,21 +18,8 @@ class UsersGUIServiceProvider extends ServiceProvider
     {
         $groupsBase = 'ice-users-gui';
         $groups = "$groupsBase-migrations";
-        $global = $GLOBALS;
-        $isPublish = isset($global['argv']) && isset($global['argv'][0]) && $global['argv'][0] === 'artisan'
-            && isset($global['argv'][1]) && $global['argv'][1] === 'vendor:publish'
-            && isset($global['argv'][2]) && $global['argv'][2] === "--tag=$groups";
 
-        $files = File::files(__DIR__ . '/database/migrations/');
-        $sortedFiles = collect($files)->sortBy(fn($file) => $file->getFilename())->values();
-
-        $paths = [];
-        foreach ($sortedFiles as $v) {
-            $fileInfo = $v->getFileInfo();
-            $publishAs = MigrationHelper::publishAs($v, $isPublish, 2);
-
-            $paths[$fileInfo->getPathname()] = $publishAs;
-        }
+        $paths = MigrationHelper::stubsToMigrations($groups, __DIR__ . '/database/migrations/');
 
         $this->publishes($paths, $groups);
         $this->publishes([__DIR__ . '/resources/js' =>  resource_path("js/totocsa/$groupsBase")], "$groupsBase-resources");
